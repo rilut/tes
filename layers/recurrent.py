40edd08c1897972fb0ae06b6487eaaab8c3bcfef
Rizky Luthfianto <mrluthfianto@gmail.com>
2017-09-03 13:10:17 +0700

    [WIP] Add HighwayLSTM
    Untested

---
+++ layers/recurrent.py
@@ -0,0 +1,136 @@
+#pylint: disable=C0103,W0201
+
+from keras import backend as K
+from keras import activations, initializations, regularizers
+from keras.engine import InputSpec
+from keras.layers.recurrent import LSTM
+from keras.layers.recurrent import time_distributed_dense
+
+
+class HighwayLSTM(LSTM):
+    """ """
+    def __init__(self, output_dim,
+                 init='glorot_uniform', inner_init='orthogonal',
+                 forget_bias_init='one', activation='tanh',
+                 inner_activation='hard_sigmoid',
+                 W_regularizer=None, U_regularizer=None, b_regularizer=None,
+                 dropout_W=0., dropout_U=0., **kwargs):
+        super(HighwayLSTM, self).__init__(self, output_dim,
+                                          init, inner_init,
+                                          forget_bias_init, activation,
+                                          inner_activation,
+                                          W_regularizer, U_regularizer, b_regularizer,
+                                          dropout_W, dropout_U, **kwargs)
+
+    def build(self, input_shape):
+        super(HighwayLSTM, self).build(input_shape)
+        self.input_spec = [InputSpec(shape=input_shape)]
+        input_dim = input_shape[2]
+        self.input_dim = input_dim
+
+        if self.stateful:
+            self.reset_states()
+        else:
+            # initial states: 2 all-zero tensors of shape (output_dim)
+            self.states = [None, None]
+
+        self.W_r = self.init((input_dim, self.output_dim),
+                             name='{}_W_r'.format(self.name))
+        self.U_r = self.inner_init((self.output_dim, self.output_dim),
+                                   name='{}_U_r'.format(self.name))
+        self.b_r = K.zeros((self.output_dim,), name='{}_b_r'.format(self.name))
+
+        self.W_h = self.init((input_dim, self.output_dim),
+                             name='{}_W_h'.format(self.name))
+        self.U_h = self.inner_init((self.output_dim, self.output_dim),
+                                   name='{}_U_h'.format(self.name))
+
+        self.regularizers = []
+        if self.W_regularizer:
+            self.W_regularizer.set_param(K.concatenate([self.W_i,
+                                                        self.W_f,
+                                                        self.W_c,
+                                                        self.W_o,
+                                                        self.W_r,
+                                                        self.W_h]))
+            self.regularizers.append(self.W_regularizer)
+        if self.U_regularizer:
+            self.U_regularizer.set_param(K.concatenate([self.U_i,
+                                                        self.U_f,
+                                                        self.U_c,
+                                                        self.U_o,
+                                                        self.U_r,
+                                                        self.U_h]))
+            self.regularizers.append(self.U_regularizer)
+        if self.b_regularizer:
+            self.b_regularizer.set_param(K.concatenate([self.b_i,
+                                                        self.b_f,
+                                                        self.b_c,
+                                                        self.b_o,
+                                                        self.b_r]))
+            self.regularizers.append(self.b_regularizer)
+
+        self.trainable_weights += [self.W_r, self.U_r, self.b_r, self.W_h, self.U_h]
+
+        if self.initial_weights is not None:
+            self.set_weights(self.initial_weights)
+            del self.initial_weights
+
+    def preprocess_input(self, x, train=False):
+        if self.consume_less == 'cpu':
+            if train and (0 < self.dropout_W < 1):
+                dropout = self.dropout_W
+            else:
+                dropout = 0
+            input_shape = self.input_spec[0].shape
+            input_dim = input_shape[2]
+            timesteps = input_shape[1]
+
+            x_i = time_distributed_dense(x, self.W_i, self.b_i, dropout,
+                                         input_dim, self.output_dim, timesteps)
+            x_f = time_distributed_dense(x, self.W_f, self.b_f, dropout,
+                                         input_dim, self.output_dim, timesteps)
+            x_c = time_distributed_dense(x, self.W_c, self.b_c, dropout,
+                                         input_dim, self.output_dim, timesteps)
+            x_o = time_distributed_dense(x, self.W_o, self.b_o, dropout,
+                                         input_dim, self.output_dim, timesteps)
+            x_r = time_distributed_dense(x, self.W_r, self.b_r, dropout,
+                                         input_dim, self.output_dim, timesteps)
+            x_h = time_distributed_dense(x, self.W_h, None, dropout,
+                                         input_dim, self.output_dim, timesteps)
+            return K.concatenate([x_i, x_f, x_c, x_o, x_h, x_r, x_h], axis=2)
+        else:
+            return x
+
+    def step(self, x, states):
+        h_tm1 = states[0]
+        c_tm1 = states[1]
+        B_U = states[2]
+        B_W = states[3]
+
+        if self.consume_less == 'cpu':
+            x_i = x[:, :self.output_dim]
+            x_f = x[:, self.output_dim: 2 * self.output_dim]
+            x_c = x[:, 2 * self.output_dim: 3 * self.output_dim]
+            x_o = x[:, 3 * self.output_dim: 4 * self.output_dim]
+            x_r = x[:, 4 * self.output_dim: 5 * self.output_dim]
+            x_h = x[:, 5 * self.output_dim:]
+        else:
+            x_i = K.dot(x * B_W[0], self.W_i) + self.b_i
+            x_f = K.dot(x * B_W[1], self.W_f) + self.b_f
+            x_c = K.dot(x * B_W[2], self.W_c) + self.b_c
+            x_o = K.dot(x * B_W[3], self.W_o) + self.b_o
+            x_r = K.dot(x * B_W[4], self.W_r) + self.b_r
+            x_h = K.dot(x * B_W[5], self.W_h)
+
+        i = self.inner_activation(x_i + K.dot(h_tm1 * B_U[0], self.U_i))
+        f = self.inner_activation(x_f + K.dot(h_tm1 * B_U[1], self.U_f))
+        c = f * c_tm1 + i * self.activation(x_c + K.dot(h_tm1 * B_U[2], self.U_c))
+        o = self.inner_activation(x_o + K.dot(h_tm1 * B_U[3], self.U_o))
+
+        # Highway LSTM
+        r = self.inner_activation(x_r + K.dot(h_tm1 * B_U[4], self.U_r))
+        hh = o * self.activation(c)
+        h = r * hh + (1-r) * x_h
+
+        return h, [h, c]
